ad32eddc768f291675498ec887794cca1b057fef
ToyasDhake <toyasdhake@gmail.com>
2020-04-06 06:13:58 -0400

    Implemented car with manual control.

---
+++ Assets/Scripts/DashboardManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class DashboardManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void exitApplication()
+    {
+        Application.Quit();
+    }
+
+    public void loadManualCarControl()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
---
+++ Assets/Scripts/Mudguard.cs
@@ -0,0 +1,23 @@
+using System;
+using UnityEngine;
+
+
+    public class Mudguard : MonoBehaviour
+    {
+        public CarManualController carController; // car controller to get the steering angle
+
+        private Quaternion m_OriginalRotation;
+
+
+        private void Start()
+        {
+            m_OriginalRotation = transform.localRotation;
+        }
+
+
+        private void Update()
+        {
+            transform.localRotation = m_OriginalRotation*Quaternion.Euler(0, carController.CurrentSteerAngle, 0);
+        }
+    
+}
